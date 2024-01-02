4d9d92bac6047e1332b8e737d1443f5b6f46491e
Prashant Kumar Yadav <yprashant174@gmail.com>
2024-01-02 13:47:00 +0530

    Routing , signup login and middleware authentication complete

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const { userRouter } = require("./routes/userRoutes")
+const { connection } = require("./config/db")
+const { authenticate } = require("./middleware/authentication")
+const app=express()
+require("dotenv").config()
+app.use(express.json())
+
+app.get("/",authenticate,(req,res)=>{
+    res.status(200).send("charitsm")
+})
+
+app.use("/",userRouter)
+
+const port=process.env.PORT||8080
+app.listen(port,async()=>{
+    try{
+       await connection
+       console.log("mongodb connected")
+    }catch(err){
+        console.log(err)
+    }
+    console.log(`server running on port ${port}`)
+})
